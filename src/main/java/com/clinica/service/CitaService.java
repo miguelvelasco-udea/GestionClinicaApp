d168414fb8ba88ca2eef1db799ac83998df75339
salometoroc-cmd <salome.toroc@udea.edu.co>
2025-10-21 15:08:57 -0500

    Update CitaService.java

--- src/main/java/com/clinica/service/CitaService.java
+++ src/main/java/com/clinica/service/CitaService.java
@@ -1,5 +1,54 @@
 package com.clinica.service;
+import com.clinica.model.Cita;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CitaService {
+public class CitaService implements ICitaService {
+
+    private List<Cita> citas =new ArrayList<>();
+    private int contadorId = 1;
+
+    @Override
+    public void crearCita(Cita cita){
+        cita.setId(contadorId++);
+        citas.add(cita);
+        
+    }
     
+    @Override
+
+    public void cancelarCita(int id){
+        Cita cita = buscarPorId(id);
+        if (cita != null){
+            cita.setEstado(com.clinica.model.EstadoCita.CANCELADA);
+        }
+    }
+    
+    @Override 
+    public void reprogramarCita(int id, LocalDate nuevaFecha){
+        Cita cita = buscarPorId(id);
+        if (cita != null){
+            cita.setFecha(nuevaFecha);
+        }
+
+    }
+
+    @Override 
+    public Cita buscarPorId(int id) {
+        return citas.stream()
+                    .filter(c -> c.getId() == id)
+                    .findFirst()
+                    .orElse(null);
+
+    }
+
+    @Override 
+    public List<Cita> listarCitas() {
+        return citas;
+    }
 }
+
+
+    
+
